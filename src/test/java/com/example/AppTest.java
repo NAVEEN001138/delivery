c51e9a3d46369a0ae71c732d111808ae3d910b57
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-13 13:17:03 +0530

    fdsa

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,10 +1,10 @@
-package com.example;
-import static org.junit.Assert.assertTrue;
+package com.example; // MUST MATCH
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest {
     @Test
-    public void testIsPrime() {
-        assertTrue(App.isPrime(7));
+    public void testApp() {
+        assertTrue(App.isPrime(7)); // It can only see 'App' if packages match
     }
 }
